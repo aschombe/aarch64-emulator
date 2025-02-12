74787e493c95789589ef35d29b180235073edeed
rmonaghanjr <rmonagha@protonmail.com>
2025-02-12 13:42:32 -0500

    dologm is a function now

--- include/util/Logging.h
+++ include/util/Logging.h
@@ -13,18 +13,12 @@ typedef enum {
 } LogLevel;
 
 #ifndef LOG_MASK
-#define LOG_MASK = 0xf;
+#define LOG_MASK 0xf
 #endif
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 const char* stringOfLogLevel(LogLevel level);
 
-#define dologm(mask, ...) if ((mask & LOG_MASK) != 0) { \
-    time_t t = time(NULL); \
-    struct tm now = *localtime(&t); \
-    char dtbuf[50] = {0}; \
-    sprintf(dtbuf, "%02d/%02d/%02d, %02d:%02d:%02d", now.tm_mon + 1, now.tm_mday, now.tm_year + 1900, now.tm_hour, now.tm_min, now.tm_sec); \
-    printf("%-8s [%s] %25s:%-10d ", stringOfLogLevel(mask), dtbuf, __FILENAME__, __LINE__); \
-    printf(__VA_ARGS__); \
-}
+void dologm(LogLevel mask, const char* format, ...);
+
 #define dolog(...) dologm(STATUS, __VA_ARGS__)
--- src/util/Logging.cpp
+++ src/util/Logging.cpp
@@ -1,4 +1,5 @@
 #include "util/Logging.h"
+#include <stdarg.h>
 
 const char* stringOfLogLevel(LogLevel level) {
     switch (level) {
@@ -9,4 +10,20 @@ const char* stringOfLogLevel(LogLevel level) {
     }
     
     return "UNKNOWN";
+}
+
+void dologm(LogLevel mask, const char* format, ...) {
+    if ((mask & LOG_MASK) == 0) return;
+
+    va_list args;
+    va_start(args, format);
+
+    time_t t = time(NULL);
+    struct tm now = *localtime(&t);
+    char dtbuf[50] = {0};
+    sprintf(dtbuf, "%02d/%02d/%02d, %02d:%02d:%02d", now.tm_mon + 1, now.tm_mday, now.tm_year + 1900, now.tm_hour, now.tm_min, now.tm_sec);
+    printf("%-8s [%s] %25s:%-10d ", stringOfLogLevel(mask), dtbuf, __FILENAME__, __LINE__);
+    vprintf(format, args);
+
+    va_end(args);
 }
